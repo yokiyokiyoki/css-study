8f41f9733ff434ac9067e99d3d835caff0eea688
youjie <youjie@hudongpai.com>
2018-02-08 15:59:22 +0800

    侧边栏初始化

--- src/pages/home/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="home">12</div>
-</template>
-<script>
-    export default {};
-</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <el-container class="home">
+        <el-aside width="200px">
+            <el-menu
+            default-active="1"
+            class="el-menu-vertical-demo"
+            @open="handleOpen">
+            <el-menu-item index="1">
+                <span slot="title">导航二</span>
+            </el-menu-item>
+            </el-menu>
+        </el-aside>
+        <el-main>
+            <router-view />
+        </el-main>
+    </el-container>
+</template>
+<script>
+    export default {
+      methods: {
+        handleSelect(key, keyPath) {
+          console.log(key, keyPath);
+        }
+      }
+    };
+</script>
+<style lang="less" scoped>
+    .home {
+      height: 100%;
+      .el-menu {
+        height: 100%;
+      }
+    }
+
+</style>
