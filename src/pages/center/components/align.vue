1447af11764f01c5f2a94268a9f9ab1e43fa2428
youjie <youjie@hudongpai.com>
2018-02-09 15:14:40 +0800

    垂直居中第一种方式

--- src/pages/center/components/align.vue
+++ src/pages/center/components/align.vue
@@ -1,7 +1,36 @@
 <template>
-    <div>1</div>
+    <div>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <div class="title">- 若盒子内部元素是单行文本，则设置其line-height等于其父级元素的高度</div>
+            </div >
+            <div class="item">
+                <div class="wrap-one">
+                    <div class="box">居中文本</div>
+                </div>
+            </div>
+        </el-card>
+    </div>
 </template>
 <script>
     export default {};
 </script>
+<style lang="less" scoped>
+    .box-card {
+      .warn {
+        color: #f56c6c;
+      }
+      margin: 10px 0px;
+      .title {
+        padding: 14px 0;
+      }
+      .wrap-one {
+        height: 30px;
+        .box {
+          line-height: 30px;
+        }
+      }
+    }
+
+</style>
 
