560ed40576125fe64d73d5cc0278557a831a24be
youjie <youjie@hudongpai.com>
2018-02-13 14:48:50 +0800

    垂直居中

--- src/pages/center/components/align.vue
+++ src/pages/center/components/align.vue
@@ -49,13 +49,50 @@
         </el-card>
         <el-card class="box-card">
             <div slot="header" class="clearfix">
-                <div class="title">-  vertical-align</div>
+                <div class="title">- vertical-align</div>
                 <div class="title">- vertical-align只有在父层是td/th时，才会生效，对于其他块状元素，div/p是不生效的</div>
                 <div class="title">- 为了使用vertical-align，需要设定父级元素display:table,子元素display:table-cell;vertical-align:middle;</div>
                 <div class="title">- 元素高度不定</div>
             </div >
             <div class="item">
                 <div class="wrap-five">
+                    <div class="box">居中文本</div>
+                </div>
+            </div>
+        </el-card>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <div class="title">- 元素高度固定</div>
+                <div class="title">- 使用绝对定位和margin-top</div>
+                <div class="title">- margin-top:-0.5高度</div>
+                <div class="title">- 该元素高16px，故而margin-top是-8px</div>
+                <div class="title">- 父元素空间不够时, 子元素可能不可见(当浏览器窗口缩小时,滚动条不出现时)。如果子元素设置了overflow:auto, 则高度不够时, 会出现滚动条</div>
+            </div >
+            <div class="item">
+                <div class="wrap-six">
+                    <div class="box">居中文本</div>
+                </div>
+            </div>
+        </el-card>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <div class="title">- 元素高度固定</div>
+                <div class="title">- 使用绝对定位和margin:auto 0;</div>
+                <div class="title">- 该元素高为16px</div>
+                <div class="title">- 没有足够空间时, 子元素会被截断, 但不会有滚动条</div>
+            </div >
+            <div class="item">
+                <div class="wrap-seven">
+                    <div class="box">居中文本</div>
+                </div>
+            </div>
+        </el-card>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <div class="title">- grid布局</div>
+            </div >
+            <div class="item">
+                <div class="wrap-eight">
                     <div class="box">居中文本</div>
                 </div>
             </div>
@@ -112,6 +149,33 @@
         .box {
           display: table-cell;
           vertical-align: middle;
+        }
+      }
+      .wrap-six {
+        position: relative;
+        height: 30px;
+        .box {
+          position: absolute;
+          top: 50%;
+          margin-top: -8px;
+        }
+      }
+      .wrap-seven {
+        position: relative;
+        height: 30px;
+        .box {
+          position: absolute;
+          margin: auto 0;
+          top: 0;
+          bottom: 0;
+          height: 16px;
+        }
+      }
+      .wrap-eight {
+        display: grid;
+        height: 30px;
+        .box {
+          align-self: center;
         }
       }
     }
