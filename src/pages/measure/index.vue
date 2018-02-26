a419e03af3f38ef019afdee37aa683d366d34d0d
李友杰 <liyoujie@liyoujiedeMacBook-Pro.local>
2018-02-26 21:14:42 +0800

    提交em

--- src/pages/measure/index.vue
+++ src/pages/measure/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="measure"></div>
+  <div class="measure">
+      <el-card class="box-card">
+            <div slot='header'>
+                <div class="title">- 相对性单位：em</div>
+                <div class="title">- 是指所属对象内的文本大小</div>
+                <div class="title">- body默认的字体为16px，如果给他设置14px，他的任意子元素div的1.2em是14*1.2=16px</div>
+                <div class="title">- 如果用em层层嵌套下去，每一个div都从他父级元素那里继承字体大小</div>
+                <div class="title">- 可以看到它是一层层都是乘以1.2的</div>
+                <div class="title">- 而如果父级元素不是em，就照父级元素的px来</div>
+            </div>
+            <div class="item">
+                <div class="wrap-em" style="font-size:14px;">
+                    <div style="font-size:1.2em">
+                        test1(14*1.2px)
+                        <div style="font-size:1.2em">test2(14*1.2*1.2px)</div>
+                    </div>
+                </div>
+                <div class="wrap-em" style="font-size:14px;">
+                    <div>
+                        test1（14px）
+                        <div style="font-size:1.2em">test2(14*1.2px)</div>
+                    </div>
+                </div>
+            </div>    
+        </el-card> 
+  </div>
 </template>
 <script>
 export default {
