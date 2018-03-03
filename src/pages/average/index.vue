3ed04e3d88c63d1f02a14a2434af6fe463e9619c
youjie <youjie@hudongpai.com>
2018-03-03 14:07:38 +0800

    flex

--- src/pages/average/index.vue
+++ src/pages/average/index.vue
@@ -1,6 +1,45 @@
 <template>
-  <div class="average">1</div>
+  <div class="average">
+      <el-card class="box-card">
+            <div slot='header'>
+                <div class="title">- 均等分配，中间有空隙</div>
+                <div class="title">- 使用flex布局</div>
+            </div>
+            <div class="item">
+                <div class="wrap-flex">
+                    <div class="col border-box">1</div>
+                    <div class="col border-box">2</div>
+                    <div class="col border-box">3</div>
+                    <div class="col border-box">4</div>
+                </div>
+            </div>  
+        </el-card>
+  </div>
 </template>
 <script>
 export default {};
 </script>
+<style lang="less" scoped>
+.border-box {
+  border: 1px solid black;
+}
+.box-card {
+  width: 100%;
+  margin: 10px 0px;
+  .title {
+    text-align: left;
+    padding: 10px;
+  }
+}
+.wrap-flex {
+  display: flex;
+  height: 100px;
+  width: 430px;
+  .col {
+    flex: 1;
+    & + .col {
+      margin-left: 10px;
+    }
+  }
+}
+</style>
