931c624a3d0d5b3e09b7355fa5e13ec8b3d0e136
youjie <youjie@hudongpai.com>
2018-03-05 10:18:20 +0800

    float

--- src/pages/average/index.vue
+++ src/pages/average/index.vue
@@ -14,6 +14,31 @@
                 </div>
             </div>  
         </el-card>
+        <el-card class="box-card">
+            <div slot='header'>
+                <div class="title">- 均等分配，中间有空隙</div>
+                <div class="title">- 使用float</div>
+                <div class="title">- 再利用box-sizing:border-box;控制宽度</div>
+                <div class="title">- 先给wrap设个负边距</div>
+                <div class="title">- 但是比里面多了标签</div>
+            </div>
+            <div class="item">
+                <div class="wrap-float">
+                    <div class="col border-box">
+                        <p>1</p>
+                    </div>
+                    <div class="col border-box">
+                        <p>2</p>
+                    </div>
+                    <div class="col border-box">
+                        <p>3</p>
+                    </div>
+                    <div class="col border-box">
+                        <p>4</p>
+                    </div>
+                </div>
+            </div>  
+        </el-card>
   </div>
 </template>
 <script>
@@ -39,6 +64,20 @@ export default {};
     flex: 1;
     & + .col {
       margin-left: 10px;
+    }
+  }
+}
+.wrap-float {
+  width: 430px;
+  height: 100px;
+  margin-left: -10px;
+  .col {
+    width: 25%;
+    padding-left: 10px;
+    box-sizing: border-box;
+    float: left;
+    p {
+      background-color: red;
     }
   }
 }
