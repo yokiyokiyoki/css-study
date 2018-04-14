c07a621b3771a35ae3db928f9788d9cb5fad19f5
youjie <youjie@hudongpai.com>
2018-04-14 12:53:14 +0800

    伪元素提交

--- src/pages/after/index.vue
+++ src/pages/after/index.vue
@@ -4,6 +4,15 @@
             <div slot='header'>
                 <div class="title">- 鼠标悬浮实现一个提示的文字</div>
                 <div class="title">- 不能用JavaScript</div>
+                <div class="title">- 伪元素的content一般为了做一些效果，都是直接留空</div>
+                <div class="title">- 这里可以content:attr(data-msg)抓去attr上的内容</div>
+                <div class="title">- 也可以配合属性选择器</div>
+                <div class="title">- 显示没有文本值但是 href 属性具有链接的 a 元素的链接</div>
+            </div>
+            <div class="item">
+                <div class="hover-item" data-msg='我是attr里带的信息'>hover</div>
+                <a href="http://baidu.com">我是文本值链接</a>
+                <a href="http://baidu.com"></a>
             </div>
         </el-card>
   </div>
@@ -21,6 +30,25 @@ export default {};
   .title {
     text-align: left;
     padding: 10px;
+  }
+  .item {
+    .hover-item {
+      position: relative;
+      width: 100px;
+      &:hover::after {
+        content: attr(data-msg);
+        position: absolute;
+        font-size: 12px;
+        width: 100px;
+        text-align: center;
+        left: 0;
+        top: -30px;
+        border: 1px solid black;
+      }
+    }
+    a[href^="h"]:empty::after {
+      content: attr(href);
+    }
   }
 }
 </style>
