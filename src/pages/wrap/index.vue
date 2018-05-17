a26f867d5e68024c847b57791534d7744d1eb1d1
youjie <youjie@hudongpai.com>
2018-05-17 21:49:40 +0800

    换行

---
+++ src/pages/wrap/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="wrap">
+        <el-card class="box-card">
+            <div class="item" >
+               
+            </div>
+        </el-card>
+    </div>
+</template>
+<script>
+    export default {
+      data() {
+        return {
+          
+        };
+      }
+    };
+</script>
+<style lang="less" scoped>
+    .wrap {
+      text-align: left;
+      .item {
+        padding: 18px 0;
+      }
+
+      .box-card {
+        width: 100%;
+      }
+    }
+
+</style>
+
